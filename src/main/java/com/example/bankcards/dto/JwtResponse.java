052fd84136a61c5ff2177531ed74dbfd4bd3c8c1
Vitaliy Borodin <kiverak@gmail.com>
2025-09-09 21:15:13 +0300

    create jwt & security

---
+++ src/main/java/com/example/bankcards/dto/JwtResponse.java
@@ -0,0 +1,3 @@
+package com.example.bankcards.dto;
+
+public record JwtResponse(String token) {}
---
+++ src/main/java/com/example/bankcards/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.entity.User;
+import com.example.bankcards.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles().stream()
+                        .map(role -> "ROLE_" + role.getName())
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toSet()))
+                .accountLocked(user.getStatus() == User.UserStatus.BLOCKED)
+                .build();
+    }
+}
