4483b656fe1956edc7c5e077b28c79a928cbe29c
Vitaliy Borodin <kiverak@gmail.com>
2025-09-09 21:14:14 +0300

    create repositories

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,17 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CardRepository extends JpaRepository<Card, UUID> {
+
+    Page<Card> findByOwner(User owner, Pageable pageable);
+
+    Optional<Card> findByIdAndOwner(UUID id, User owner);
+}
